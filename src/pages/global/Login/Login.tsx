924787701f9678f7035aba6d45e7ce7d17db0354
Caio Alcarria Martins dos Santos <alcarriacaio@gmail.com>
2025-06-19 01:27:59 -0300

    Adiciona controle de overflow no corpo da página de Login para evitar rolagem ao abrir o modal.

--- src/pages/global/Login/Login.tsx
+++ src/pages/global/Login/Login.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 const Login = () => {
   const [isOpen, setIsOpen] = useState(false);
+  document.body.style.overflow = 'hidden';
   return (
     <Container>
       <Logo>
